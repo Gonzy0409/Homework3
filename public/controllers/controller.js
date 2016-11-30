43743e85856fde74586aa754025e12ce5af8d8cf
Gonzalo <sergon0409@gmail.com>
2016-11-29 22:11:25 -0800

    push to github

---
+++ public/controllers/controller.js
@@ -0,0 +1,40 @@
+var myApp = angular.module('myApp', []); 
+
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+    var refresh = function(){
+	    $http.get('/contactlist').then(function(response){
+	    	$scope.contactlist = response.data;
+	    	$scope.contact = {};
+	    });
+	};
+
+	refresh();
+
+    $scope.addContact = function(){
+    	$http.post('/contactlist', $scope.contact).then(function(response){
+    		refresh();
+    	});
+    };
+
+    $scope.remove = function(id){
+    	$http.delete('/contactlist/' + id).then(function(response){
+    		refresh();
+    	});
+    };
+
+    $scope.edit = function(id){
+    	$http.get('/contactlist/' + id).then(function(response){
+    		$scope.contact = response.data;
+    	});
+    };
+
+    $scope.update = function(){
+    	$http.put('/contactlist/' + $scope.contact._id, $scope.contact).then(function(response){
+    		refresh();
+    	});
+    }
+
+    $scope.deselect = function(){
+    	$scope.contact = {};
+    }
+}]);

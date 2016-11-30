a07e400488e91b0c2f0d296deec78dad6d162871
Gonzalo <sergon0409@gmail.com>
2016-11-29 23:13:25 -0800

    modified contact list app for homework 3

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,39 +2,10 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     var refresh = function(){
-	    $http.get('/contactlist').then(function(response){
-	    	$scope.contactlist = response.data;
-	    	$scope.contact = {};
+	    $http.get('/Homework3').then(function(response){
+	    	$scope.resume = response.data;
 	    });
 	};
 
 	refresh();
-
-    $scope.addContact = function(){
-    	$http.post('/contactlist', $scope.contact).then(function(response){
-    		refresh();
-    	});
-    };
-
-    $scope.remove = function(id){
-    	$http.delete('/contactlist/' + id).then(function(response){
-    		refresh();
-    	});
-    };
-
-    $scope.edit = function(id){
-    	$http.get('/contactlist/' + id).then(function(response){
-    		$scope.contact = response.data;
-    	});
-    };
-
-    $scope.update = function(){
-    	$http.put('/contactlist/' + $scope.contact._id, $scope.contact).then(function(response){
-    		refresh();
-    	});
-    }
-
-    $scope.deselect = function(){
-    	$scope.contact = {};
-    }
 }]);
